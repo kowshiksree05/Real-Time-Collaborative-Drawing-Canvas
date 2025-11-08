c17533f1871ce4fb08b6e31e891c4e3993d33c0a
kowshiksree05 <160203436+kowshiksree05@users.noreply.github.com>
2025-11-08 14:19:15 +0530

    Delete Real-Time Collaborative Drawing Canvas Project directory

--- Real-Time Collaborative Drawing Canvas Project/dist/server/rooms.d.ts
+++
@@ -1,9 +0,0 @@
-import { User } from './types';
-export declare class RoomManager {
-    private userColorIndex;
-    generateUserId(): string;
-    generateUserName(): string;
-    assignUserColor(): string;
-    createUser(name?: string): User;
-}
-//# sourceMappingURL=rooms.d.ts.map
